8c362183bffd843c5af09c9d11e4be893b06e0f8
Alena Grechka <alyonagrechka1@gmail.com>
2023-03-12 13:19:18 +0000

    step 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
@@ -29,20 +31,45 @@ const apiUrl = 'http://127.0.0.1:5500/api/people.json';
 
 @immutable
 class Person {
+  final String id;
   final String name;
   final int age;
+  final String imageUrl;
+  final Uint8List? imageData;
+  final bool isLoading;
+
+  Person copiedWith([
+    bool? isLoading,
+    Uint8List? imageData,
+  ]) =>
+      Person(
+        id: id,
+        name: name,
+        age: age,
+        imageUrl: imageUrl,
+        imageData: imageData ?? this.imageData,
+        isLoading: isLoading ?? this.isLoading,
+      );
 
   const Person({
+    required this.id,
     required this.name,
     required this.age,
+    required this.imageUrl,
+    required this.imageData,
+    required this.isLoading,
   });
 
   Person.fromJson(Map<String, dynamic> json)
-      : name = json['name'] as String,
-        age = json['age'] as int;
+      : id = json['id'] as String,
+        name = json['name'] as String,
+        age = json['age'] as int,
+        imageUrl = json['image_url'] as String,
+        imageData = null,
+        isLoading = false;
 
   @override
-  String toString() => 'Person ($name, $age years old)';
+  String toString() => 'Person (id = $id, $name, $age years old)';
 }
 
 Future<Iterable<Person>> getPersons() => HttpClient()
@@ -74,10 +101,34 @@ class FailedToFetchPeopleAction extends Action {
 }
 
 @immutable
+class LoadPersonImageAction extends Action {
+  final String personId;
+  const LoadPersonImageAction({required this.personId});
+}
+
+@immutable
+class SuccessfullyLoadedPersonImageAction extends Action {
+  final String personId;
+  final Uint8List imageData;
+
+  const SuccessfullyLoadedPersonImageAction({
+    required this.personId,
+    required this.imageData,
+  });
+}
+
+@immutable
 class State {
   final bool isLoading;
   final Iterable<Person>? fetchedPersons;
   final Object? error;
+
+  Iterable<Person>? get sortedFetchedPersons => fetchedPersons?.toList()
+    ?..sort(
+      ((a, b) => int.parse(a.id).compareTo(
+            int.parse(b.id),
+          )),
+    );
 
   const State({
     required this.isLoading,
@@ -92,7 +143,44 @@ class State {
 }
 
 State reducer(State oldState, action) {
-  if (action is LoadPeopleAction) {
+  if (action is SuccessfullyLoadedPersonImageAction) {
+    final person = oldState.fetchedPersons?.firstWhere(
+      (p) => p.id == action.personId,
+    );
+    if (person != null) {
+      return State(
+        isLoading: false,
+        error: oldState.error,
+        fetchedPersons: oldState.fetchedPersons
+            ?.where((p) => p.id != person.id)
+            .followedBy([
+          person.copiedWith(
+            false,
+            action.imageData,
+          )
+        ]),
+      );
+    } else {
+      // person is null
+      return oldState;
+    }
+  } else if (action is LoadPersonImageAction) {
+    final person = oldState.fetchedPersons?.firstWhere(
+      (p) => p.id == action.personId,
+    );
+    if (person != null) {
+      return State(
+        isLoading: false,
+        error: oldState.error,
+        fetchedPersons: oldState.fetchedPersons
+            ?.where((p) => p.id != person.id)
+            .followedBy([person.copiedWith(true)]),
+      );
+    } else {
+      // person is null
+      return oldState;
+    }
+  } else if (action is LoadPeopleAction) {
     return const State(
       error: null,
       fetchedPersons: null,
@@ -129,6 +217,29 @@ void loadPeopleMiddleware(
   next(action);
 }
 
+void loadPersonImageMiddleware(
+  Store<State> store,
+  action,
+  NextDispatcher next,
+) {
+  if (action is LoadPersonImageAction) {
+    final person = store.state.fetchedPersons?.firstWhere(
+      (p) => p.id == action.personId,
+    );
+    if (person != null) {
+      final url = person.imageUrl;
+      final bundle = NetworkAssetBundle(Uri.parse(url));
+      bundle.load(url).then((bd) => bd.buffer.asUint8List()).then((data) {
+        store.dispatch(
+          SuccessfullyLoadedPersonImageAction(
+              personId: person.id, imageData: data),
+        );
+      });
+    }
+  }
+  next(action);
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -139,7 +250,10 @@ class MyHomePage extends StatelessWidget {
     final store = Store(
       reducer,
       initialState: const State.initial(),
-      middleware: [loadPeopleMiddleware],
+      middleware: [
+        loadPeopleMiddleware,
+        loadPersonImageMiddleware,
+      ],
     );
     return Scaffold(
       appBar: AppBar(
@@ -166,7 +280,7 @@ class MyHomePage extends StatelessWidget {
               },
             ),
             StoreConnector<State, Iterable<Person>?>(
-              converter: (store) => store.state.fetchedPersons,
+              converter: (store) => store.state.sortedFetchedPersons,
               builder: (context, people) {
                 if (people == null) {
                   return const SizedBox();
@@ -176,9 +290,36 @@ class MyHomePage extends StatelessWidget {
                     itemCount: people.length,
                     itemBuilder: (context, index) {
                       final person = people.elementAt(index);
+
+                      final infoWidget = Text('${person.age} years old');
+
+                      final Widget subtitle = person.imageData == null
+                          ? infoWidget
+                          : Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                infoWidget,
+                                Image.memory(person.imageData!)
+                              ],
+                            );
+
+                      final Widget trailing = person.isLoading
+                          ? const CircularProgressIndicator()
+                          : TextButton(
+                              onPressed: () {
+                                store.dispatch(
+                                  LoadPersonImageAction(
+                                    personId: person.id,
+                                  ),
+                                );
+                              },
+                              child: const Text('Load image'),
+                            );
+
                       return ListTile(
                         title: Text(person.name),
-                        subtitle: Text('${person.age} years old'),
+                        subtitle: subtitle,
+                        trailing: trailing,
                       );
                     },
                   ),
